54c68f329c701a91980092cb8d0476492aa01588
vetal <semeniv09@ukr.net>
2025-07-28 13:19:30 +0000

    task-rda_3_transactions

--- task.sql
+++ task.sql
@@ -1,11 +1,20 @@
--- Use our database
-USE ShopDB; 
 
--- Some data should be created outside the transaction (here)
+START TRANSACTION;
 
--- Start the transaction 
-START TRANSACTION; 
 
--- And some data should be created inside the transaction 
+INSERT INTO Orders (CustomerID, Date)
+VALUES (1, '2023-01-01');
 
-COMMIT; 
+SET @order_id = LAST_INSERT_ID();
+
+лення
+INSERT INTO OrderItems (OrderID, ProductID, Count)
+VALUES (@order_id, 1, 1);
+
+
+UPDATE Products
+SET WarehouseAmount = WarehouseAmount - 1
+WHERE ID = 1;
+
+
+COMMIT;
